8c1eafe08c2782678386ef8a4ec88318e546fdb4
pokolga <pokolga1801@gmail.com>
2025-04-11 13:00:40 +0300

    feat: 3 passing

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -17,8 +17,15 @@
  * 0    => promise that will be fulfilled
  * 1    => promise that will be fulfilled
  */
-function getPromise(/* number */) {
-  throw new Error('Not implemented');
+function getPromise(number) {
+  const promise = new Promise((resolve, reject) => {
+    if (number < 0) {
+      reject();
+    } else {
+      resolve();
+    }
+  });
+  return promise;
 }
 
 /**
@@ -33,8 +40,8 @@ function getPromise(/* number */) {
  * Promise.resolve('success') => promise that will be fulfilled with 'success' value
  * Promise.reject('fail')     => promise that will be fulfilled with 'fail' value
  */
-function getPromiseResult(/* source */) {
-  throw new Error('Not implemented');
+function getPromiseResult(source) {
+  return source.then(() => 'success').catch(() => 'fail');
 }
 
 /**
@@ -50,8 +57,14 @@ function getPromiseResult(/* source */) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return Promise.allSettled(promises).then((results) => {
+    const fulfilled = results.filter((result) => result.status === 'fulfilled');
+    if (fulfilled.length === 0) {
+      return Promise.reject(new Error('All promises rejected'));
+    }
+    return fulfilled[0].value;
+  });
 }
 
 /**
